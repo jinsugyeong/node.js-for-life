3818f76860cd1acf3c6478fb0dd3b7398eb15311
jinsugyeong <apr15th@naver.com>
2023-12-05 22:20:06 +0900

    자바스크립트 문법 - 변수

---
+++ web1_html_internet-master/syntax/template.js
@@ -0,0 +1,23 @@
+var name = 'gangsuter';
+var letter = 'Dear ' + name + '\
+\
+미안해 마, 독하게 날 버리고 떠나도 돼\
+네가 원한다면, 그래, good-bye \
+허나 내 맘까지 접은 건 아냐 \
+내 사랑이 이겨 \
+\
+아이야, 먼저 가  \
+어기야 디여라차 어기야디야 되찾을 거야 \
+잠시야 앞서도 널 따라잡으리 난 \
+그녀를 지켜라 날 잊지 못하게 \
+내 님이 계신 곳 끝까지 가련다'
+
++ name;
+
+//console.log(letter);
+
+
+var letter2 = `Dear ${name}\n\nHello It'me wo xing xi...`;
+console.log(letter2);
+
+
