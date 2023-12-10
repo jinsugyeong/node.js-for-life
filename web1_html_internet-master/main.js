113d404765bbb3b74a1128030fa59c9aa62cf80c
jinsugyeong <apr15th@naver.com>
2023-12-10 15:24:42 +0900

    App - 글 목록 출력하기

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -8,36 +8,19 @@ var app = http.createServer(function(request, response) {
   
     
   if(pathname === '/') {                //1. 루트일 때 실행(오류X)
-    if(queryData.id === undefined) {    //2. 쿼리 스트링 없을 때 실행 (홈 O)
-      var title = 'Welcome';
-      var description = 'Hello, Node.js';
-      var template =`
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>
-          ${description}  <!-- 읽어온 파일 내용 표시 -->
-        </p>
-      </body>
-      </html>
-      `;
-      response.writeHead(200);
-      response.end(template); //template 문자열 응답
+    if(queryData.id === undefined) {    //2. 쿼리 스트링 없을 때 실행 (홈 O)         
+      fs.readdir('./data',function(error, filelist) {
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';   
 
-    }else {       //3. 쿼리 스트링 있을 때 실행 (홈 X)
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-        var title = queryData.id;        
+        var i = 0;
+        var list = '<ul>';
+        while(i < filelist.length) {
+          list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i++;
+        }
+        list += '</ul>';
+
         var template =`
         <!doctype html>
         <html>
@@ -47,20 +30,49 @@ var app = http.createServer(function(request, response) {
         </head>
         <body>
           <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
+          ${list}
           <h2>${title}</h2>
-          <p>
-            ${description}  <!-- 읽어온 파일 내용 표시 -->
-          </p>
+          <p>${description}</p> <!-- 읽어온 파일 내용 표시 -->
         </body>
         </html>
         `;
+
         response.writeHead(200);
         response.end(template); //template 문자열 응답
+      });
+
+    }else {       //3. 쿼리 스트링 있을 때 실행 (홈 X)
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        fs.readdir('./data',function(error, filelist) {
+          var title = queryData.id;
+
+          var i = 0;
+          var list = '<ul>';
+          while(i < filelist.length) {
+            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i++;
+          }
+          list += '</ul>';
+
+          var template =`
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p> <!-- 읽어온 파일 내용 표시 -->
+          </body>
+          </html>
+          `;
+
+          response.writeHead(200);
+          response.end(template); //template 문자열 응답
+        });
       });
     }
 
