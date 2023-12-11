3fd889dd6ad6c42204f9b27b0e24438cee515486
jinsugyeong <apr15th@naver.com>
2023-12-11 22:27:26 +0900

    App - 글 삭제하기

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -27,13 +27,36 @@ var app = http.createServer(function(request, response) {
           var list = templateList(filelist);
           var template =templateHTML(title, list,
             `<h2>${title}</h2><p>${description}</p>`,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
-
+            `<a href="/create">create</a>
+             <a href="/update?id=${title}">update</a>
+             <form id="deleteForm" action="/delete_process" method="POST">
+               <input type="hidden" name="id" value="${title}" />
+               <input type="button" value="delete" id="deleteBtn" />
+             </form>`);
           response.writeHead(200);
           response.end(template); 
         });
       });
     }
+
+
+  } else if(pathname === '/delete_process') {    //delete_process일 때 실행
+    var body = '';
+
+    request.on('data', function(data) {
+      body = body + data;
+    });
+
+    request.on('end', function(data) {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function(err){
+        response.writeHead(302, {Location: `/`});
+        response.end();
+      });
+    });
+    
+    
   } else if(pathname === '/update') {    //update일 때 실행
     fs.readdir('./data',function(error, filelist) {
       fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
