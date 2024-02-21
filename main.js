dc34cbc032d578feb8b61fe8722f9e7373ba68d6
jinsugyeong <apr15th@naver.com>
2024-02-21 19:48:26 +0900

    저자 수정 기능 구현

--- main.js
+++ main.js
@@ -13,28 +13,31 @@ var app = http.createServer(function(request, response) {
   if(pathname === '/') {                      //루트일 때 실행(오류X)
     if(queryData.id === undefined) {          //쿼리 스트링 없을 때 실행 (홈 O)         
       topic.home(request, response);
+  
+  /* 글 관련 */  
     }else {                                   //쿼리 스트링 있을 때 실행 (홈 X)
       topic.page(request, response);
     }
-
+  } else if(pathname === '/create') { 
+    topic.create(request, response);
+  } else if(pathname === '/create_process') {
+    topic.create_process(request, response);
+  } else if(pathname === '/update') {         
+    topic.update(request, response);
+  } else if(pathname === '/update_process') { 
+    topic.update_process(request, response);
+  } else if(pathname === '/delete_process') { 
+    topic.delete_process(request, response);
+  
+  /* 저자 관련 */
   } else if(pathname === '/author') {
     author.home(request, response);
   } else if(pathname === '/author/create_process') {
     author.create_process(request, response);
-    
-
-  } else if(pathname === '/delete_process') { //delete_process일 때 실행
-    topic.delete_process(request, response);
-
-  } else if(pathname === '/update') {         //update일 때 실행
-    topic.update(request, response);
-  } else if(pathname === '/update_process') { //update_process일 때 실행
-    topic.update_process(request, response);
-
-  } else if(pathname === '/create') {         //create일 때 실행
-    topic.create(request, response);
-  } else if(pathname === '/create_process') { //create_process일 때 실행
-    topic.create_process(request, response);
+  } else if(pathname === '/author/update') {
+    author.update(request, response);
+  } else if(pathname === '/author/update_process') {
+    author.update_process(request, response);
 
   } else {                                    //루트가 아닐 때 실행(오류 O)
     response.writeHead(400);
