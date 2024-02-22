c09dc37b147c6f32ba13d0c8a203828f51c13fe7
jinsugyeong <apr15th@naver.com>
2024-02-23 01:09:52 +0900

    페이지 생성 구현

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 var fs = require('fs');
-var template = require('./lib/template.js');
 var path = require('path');
+var qs = require('querystring');
+var template = require('./lib/template.js');
 var sanitizeHtml = require('sanitize-html');
 
 app.get('/', function(req, res) {
@@ -42,9 +43,48 @@ app.get('/page/:pageId', function(req, res) {
   });
 });
 
+app.get('/create', function(req, res) {
+  fs.readdir('./data', function(error, filelist) {
+    var title = 'WEB - create';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list, `
+        <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+                <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+        </form>
+    `, '');
+    res.send(html);
+  });
+})
+
+//전달방식이 post기 때문에 post메서드 사용
+app.post('/create_process', function(req, res){
+  var body = '';
+  req.on('data', function(data) {
+      body = body + data;
+  });
+  req.on('end', function() {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+        res.writeHead(302, {Location: `/page/${title}`});
+        res.end();
+      });
+  });
+})
+
+
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
 })
+
+
 /* ---------------------------------------------------------
 2024-02-22  express 사용을 위한 기존 내용 주석 처리
 
