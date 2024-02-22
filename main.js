4424bcff74fe67c23026fd3d1d4c7a2c1e012480
jinsugyeong <apr15th@naver.com>
2024-02-23 02:26:25 +0900

    Express 미들웨어 사용하기 body-parser : 요청 정보의 본문 가공 compression : 압축관련

--- main.js
+++ main.js
@@ -6,8 +6,13 @@ var path = require('path');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var sanitizeHtml = require('sanitize-html');
+var bodyParser = require('body-parser');
+var compression = require('compression');
 
-app.get('/', function(req, res) {
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(compression());
+
+app.get('/', function(request, response) {
   fs.readdir('./data', function(erroer, filelist) {
     var title = 'Welcome';
     var description = 'Hello, Node.js';
@@ -16,16 +21,16 @@ app.get('/', function(req, res) {
       `<h2>${title}</h2>${description}`,
       `<a href="/create">create</a>`
       );
-      res.send(html);
+      response.send(html);
   })
 })
 
 //시맨틱 URL 처리방식 , 사용자가 요청한 URL'키:값' 형태로 가져오기
-app.get('/page/:pageId', function(req, res) {
+app.get('/page/:pageId', function(request, response) {
   fs.readdir('./data', function(error, filelist) {
-    var filteredId = path.parse(req.params.pageId).base;
+    var filteredId = path.parse(request.params.pageId).base;
     fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
-      var title = req.params.pageId;
+      var title = request.params.pageId;
       var sanitizedTitle = sanitizeHtml(title);
       var sanitizedDescription = sanitizeHtml(description, {allowedTags:['h1']});
       var list = template.list(filelist);
@@ -38,12 +43,12 @@ app.get('/page/:pageId', function(req, res) {
           <input type="submit" value="delete">
         </form>`
       );
-      res.send(html);
+      response.send(html);
     });
   });
 });
 
-app.get('/create', function(req, res) {
+app.get('/create', function(request, response) {
   fs.readdir('./data', function(error, filelist) {
     var title = 'WEB - create';
     var list = template.list(filelist);
@@ -58,24 +63,18 @@ app.get('/create', function(req, res) {
             </p>
         </form>
     `, '');
-    res.send(html);
+    response.send(html);
   });
 })
 
 //전달방식이 post기 때문에 post메서드 사용
-app.post('/create_process', function(req, res){
-  var body = '';
-  req.on('data', function(data) {
-      body = body + data;
-  });
-  req.on('end', function() {
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-        res.writeHead(302, {Location: `/page/${title}`});
-        res.end();
-      });
+app.post('/create_process', function(request, response){
+  var post = request.body;  //body-parser가 만들어 준 데이터가 들어가 있음
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+    response.writeHead(302, {Location: `/page/${title}`});
+    response.end();
   });
 })
 
@@ -106,36 +105,24 @@ app.get('/update/:pageId', function(request, response) {
 })
 
 app.post('/update_process', function(request, response) {
-  var body = '';
-  request.on('data', function(data) {
-      body = body + data;
-  });
-  request.on('end', function() {
-      var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function(error) {
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-              response.writeHead(302, {Location: `/page/${title}`});
-              response.end();
-          });
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error) {
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+          response.redirect(`/page/${title}`);
+          response.end();
       });
   });
-})
+});
 
 app.post('/delete_process', function(request, response) {
-  var body = '';
-  request.on('data', function(data) {
-      body = body + data;
-  });
-  request.on('end', function() {
-      var post = qs.parse(body);
-      var id = post.id;
-      var filteredId = path.parse(id).base;
-      fs.unlink(`data/${filteredId}`, function(error) {
-        response.redirect('/');
-      });
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function(error) {
+    response.redirect('/');
   });
 })
 
