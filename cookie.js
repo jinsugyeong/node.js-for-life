052c655f356cd5e11d6f4577c776153757c6f31c
jinsugyeong <apr15th@naver.com>
2024-02-23 23:55:22 +0900

    session과 permanent cookie

--- cookie.js
+++ cookie.js
@@ -15,11 +15,22 @@ http.createServer(function(request, response) {
     console.log(cookies.yummy_cookie);
 
     response.writeHead(200, {
-        'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=starwberry']
+        'Set-Cookie':[
+            'yummy_cookie=choco',
+            'tasty_cookie=starwberry',
+            `Permanent=cookies; Max-Age=${60*60*24*30}`
+        ]
     });
-    /*
-    response.writeHead로 Set-Cookie 했을땐 응답헤더에만 yummy_cookie=choco, tasty_cookie=starwberry가 뜨지만,
-    주석처리 후 다시 새로고침해보면 응답헤더에선 사라지고 요청헤더에 각각의 항목으로 들어가 있다.
-    */
+    //처음엔 응답헤더에만 보이지만, 주석처리 후 다시 새로고침해보면 요청헤더에만 각각의 항목으로 들어가 있다.
+    //이 후 다시 주석 풀고 실행하면 쿠키 삭제하는거 아닌 이상 계속 요청헤더 있음 
+
+
+    /**
+     *  세션 쿠키(Session cookie) : 웹 브라우저가 켜져 있는 동안에만 유효한 쿠키 > yummy, tasty
+     *  영구 쿠키(Permanent cookie) : 영속적인 쿠키로서 웹 브라우저를 종료했다가 다시 실행해도 살아있음 > Permanet
+     *      영구 쿠키는 MaxAge(유지시간(초)) 또는 Expires(만료날짜)를 정의해야한다
+     */
+    
+    
     response.end('Cookie!!');
 }).listen(3000);
